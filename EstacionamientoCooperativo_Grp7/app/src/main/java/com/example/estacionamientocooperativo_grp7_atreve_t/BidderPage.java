e44c3b38b54599b4db88962988cbbd73d87f9da5
KAREN <ptk0032037@est.univalle.edu>
2024-04-29 23:44:06 -0400

    Cambio interfaz Yerlin

---
+++ EstacionamientoCooperativo_Grp7/app/src/main/java/com/example/estacionamientocooperativo_grp7_atreve_t/BidderPage.java
@@ -0,0 +1,65 @@
+package com.example.estacionamientocooperativo_grp7_atreve_t;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.example.estacionamientocooperativo_grp7_atreve_t.ClientPagesUI.MyListGaragesPage;
+import com.example.estacionamientocooperativo_grp7_atreve_t.ClientPagesUI.ProfilePage;
+import com.example.estacionamientocooperativo_grp7_atreve_t.ClientPagesUI.RegisterGarage;
+import com.example.estacionamientocooperativo_grp7_atreve_t.ClientPagesUI.RequestBidder;
+
+
+public class BidderPage extends Activity {
+    Button btnProfileJ;
+    Button btnListGarageJ;
+    Button btnRegisterGarageJ;
+
+    Button btnRequestJ;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bidder_page);
+        btnProfileJ = findViewById(R.id.btnProfile);
+        btnListGarageJ = findViewById(R.id.btnListGarage);
+        btnRegisterGarageJ = findViewById(R.id.btnRegisterGarage);
+        btnRequestJ = findViewById(R.id.btnRequests);
+
+
+        btnProfileJ.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(BidderPage.this, ProfilePage.class);
+                startActivity(intent);
+            }
+        });
+        btnListGarageJ.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(BidderPage.this, MyListGaragesPage.class);
+                startActivity(intent);
+            }
+        });
+        btnRegisterGarageJ.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(BidderPage.this, RegisterGarage.class);
+                startActivity(intent);
+            }
+        });
+        btnRequestJ.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(BidderPage.this, RequestBidder.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
--- EstacionamientoCooperativo_Grp7/app/src/main/java/com/example/estacionamientocooperativo_grp7_atreve_t/InitPage.java
+++ EstacionamientoCooperativo_Grp7/app/src/main/java/com/example/estacionamientocooperativo_grp7_atreve_t/InitPage.java
@@ -10,17 +10,28 @@ import android.widget.Button;
 public class InitPage extends AppCompatActivity {
 
     Button btnClientJ;
+    Button btnBidderJ;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_init_page);
 
         btnClientJ= findViewById(R.id.btnClient);
+        btnBidderJ = findViewById(R.id.btnBidder);
 
         btnClientJ.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(InitPage.this, ClientPage.class);
+                startActivity(intent);
+            }
+        });
+
+        btnBidderJ.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(InitPage.this, BidderPage.class);
                 startActivity(intent);
             }
         });
